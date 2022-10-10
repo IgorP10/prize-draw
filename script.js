6428517f86879861ca3e515706afd3b970db51c9
Igor Pereira <igor60.pereira@gmail.com>
2022-10-10 01:20:46 -0300

    added

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
+function validated (min, max) {
+    console.log(min)
+}
+
 function random() {
+    
     const min = document.getElementById("minimum").value
     const max = document.getElementById("maximum").value
 
+    validated(min, max)
+    throw new Error("my error message");
     sort = Math.floor(Math.random() * max)
 
     while (sort < min) {
         sort = Math.floor(Math.random() * max)
     }
-
     document.getElementById("result").innerHTML = sort
 }
-
-function open() {
-    window.open("/result");
-}
-
-
